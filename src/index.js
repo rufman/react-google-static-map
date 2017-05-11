9bb1f520fcec39573401f0566ddffb805e942dc6
Agustin Cornu <agustinrhcp@gmail.com>
2017-05-11 13:16:08 -0300

    Add api key prop.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
-import React from 'react';
+'use strict';
 
-const values = (obj)=> {
-  return Object.keys(obj).map(key => obj[key]);
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var values = function values(obj) {
+  return Object.keys(obj).map(function (key) {
+    return obj[key];
+  });
 };
 
-const IMAGE_FORMATS = {
+var IMAGE_FORMATS = {
   // png8 or png (default) specifies the 8-bit PNG format.
   PNG: 'png',
 
@@ -18,10 +40,10 @@ const IMAGE_FORMATS = {
   JPG: 'jpg',
 
   // jpg-baseline specifies a non-progressive JPEG compression format.
-  JPG_BASELINE: 'jpg-baseline',
+  JPG_BASELINE: 'jpg-baseline'
 };
 
-const MAP_TYPES = {
+var MAP_TYPES = {
   // roadmap (default) specifies a standard roadmap image, as is normally shown on the Google Maps website.
   ROADMAP: 'roadmap',
 
@@ -33,11 +55,11 @@ const MAP_TYPES = {
 
   // hybrid specifies a hybrid of the satellite and roadmap image,
   // showing a transparent layer of major streets and place names on the satellite image.
-  HYBRID: 'hybrid',
+  HYBRID: 'hybrid'
 };
 
-const IMAGE_FORMATS_VALUES = values(IMAGE_FORMATS);
-const MAP_TYPES_VALUES = values(MAP_TYPES);
+var IMAGE_FORMATS_VALUES = values(IMAGE_FORMATS);
+var MAP_TYPES_VALUES = values(MAP_TYPES);
 
 /**
  * A wrapper for Google's Static Maps
@@ -46,110 +68,125 @@ const MAP_TYPES_VALUES = values(MAP_TYPES);
  *
  * @example: http://staticmapmaker.com/google/
  */
-class GoogleStaticMap extends React.Component {
+
+var GoogleStaticMap = function (_React$Component) {
+  _inherits(GoogleStaticMap, _React$Component);
+
+  function GoogleStaticMap() {
+    _classCallCheck(this, GoogleStaticMap);
+
+    return _possibleConstructorReturn(this, Object.getPrototypeOf(GoogleStaticMap).apply(this, arguments));
+  }
+
+  _createClass(GoogleStaticMap, [{
+    key: 'render',
+
+
+    /**
+     * https://developers.google.com/maps/documentation/staticmaps/intro#api_key
+     */
+    value: function render() {
+      return _react2.default.createElement('img', {
+        style: _extends({}, this.props.style, this.props.size),
+        src: this.staticMapUrl
+      });
+    }
+  }, {
+    key: 'staticMapUrl',
+    get: function get() {
+      var _props = this.props;
+      var latitude = _props.latitude;
+      var longitude = _props.longitude;
+      var zoom = _props.zoom;
+      var size = _props.size;
+      var scale = _props.scale;
+      var format = _props.format;
+      var mapType = _props.mapType;
+      var width = size.width;
+      var height = size.height;
+      var apiKey = _props.apiKey;
+
+      var rootUrl = this.constructor.RootUrl;
+
+      return rootUrl + '?center=' + latitude + ',' + longitude + '&zoom=' + zoom + '&scale=' + scale + '&size=' + width + 'x' + height + '&maptype=' + mapType + '&format=' + format + '&' + this.markerParams + '&key=' + apiKey;
+    }
+  }, {
+    key: 'markerParams',
+    get: function get() {
+      var _props2 = this.props;
+      var latitude = _props2.latitude;
+      var longitude = _props2.longitude;
+      var hasCenterMarker = _props2.hasCenterMarker;
+
+
+      var markerParams = 'markers=' + latitude + ',' + longitude;
+      return hasCenterMarker ? markerParams : '';
+    }
+  }, {
+    key: 'apiKeyParam',
+    get: function get() {
+      var apiKey = this.constructor.ApiKey;
+      return apiKey ? 'key=' + apiKey : '';
+    }
+  }]);
+
+  return GoogleStaticMap;
+}(_react2.default.Component);
+
+GoogleStaticMap.ApiKey = null;
+GoogleStaticMap.RootUrl = 'http://maps.googleapis.com/maps/api/staticmap';
+GoogleStaticMap.ImageFormats = IMAGE_FORMATS;
+GoogleStaticMap.MapTypes = MAP_TYPES;
+GoogleStaticMap.propTypes = {
+  latitude: _react2.default.PropTypes.string.isRequired,
+
+  longitude: _react2.default.PropTypes.string.isRequired,
+
+  size: _react2.default.PropTypes.shape({
+    width: _react2.default.PropTypes.number.isRequired,
+    height: _react2.default.PropTypes.number.isRequired
+  }),
+
+  style: _react2.default.PropTypes.object,
 
   /**
-   * https://developers.google.com/maps/documentation/staticmaps/intro#api_key
+   * zoom (required if markers not present) defines the zoom level of the map,
+   * which determines the magnification level of the map.
+   *
+   * @see https://developers.google.com/maps/documentation/staticmaps/intro#Zoomlevels
    */
-  static ApiKey = null;
+  zoom: _react2.default.PropTypes.number.isRequired,
 
-  static RootUrl = 'http://maps.googleapis.com/maps/api/staticmap';
+  /**
+   * scale affects the number of pixels that are returned.
+   * scale=2 returns twice as many pixels as scale=1 while retaining the same coverage area and level of detail
+   * The default value is calculated from the screen PixelRatio.
+   */
+  scale: _react2.default.PropTypes.number,
 
-  static ImageFormats = IMAGE_FORMATS;
+  /**
+   * @see https://developers.google.com/maps/documentation/staticmaps/intro#ImageFormats
+   */
+  format: _react2.default.PropTypes.oneOf(IMAGE_FORMATS_VALUES),
 
-  static MapTypes = MAP_TYPES;
+  /**
+   * @see https://developers.google.com/maps/documentation/staticmaps/intro#MapTypes
+   */
+  mapType: _react2.default.PropTypes.oneOf(MAP_TYPES_VALUES),
 
-  static propTypes = {
-    latitude: React.PropTypes.string.isRequired,
+  /**
+   * Add an Api key
+   */
+  apiKey: _react2.default.PropTypes.string,
 
-    longitude: React.PropTypes.string.isRequired,
-
-    size: React.PropTypes.shape({
-      width: React.PropTypes.number.isRequired,
-      height: React.PropTypes.number.isRequired,
-    }),
-
-    style: React.PropTypes.object,
-
-    /**
-     * zoom (required if markers not present) defines the zoom level of the map,
-     * which determines the magnification level of the map.
-     *
-     * @see https://developers.google.com/maps/documentation/staticmaps/intro#Zoomlevels
-     */
-    zoom: React.PropTypes.number.isRequired,
-
-    /**
-     * scale affects the number of pixels that are returned.
-     * scale=2 returns twice as many pixels as scale=1 while retaining the same coverage area and level of detail
-     * The default value is calculated from the screen PixelRatio.
-     */
-    scale: React.PropTypes.number,
-
-    /**
-     * @see https://developers.google.com/maps/documentation/staticmaps/intro#ImageFormats
-     */
-    format: React.PropTypes.oneOf(IMAGE_FORMATS_VALUES),
-
-    /**
-     * @see https://developers.google.com/maps/documentation/staticmaps/intro#MapTypes
-     */
-    mapType: React.PropTypes.oneOf(MAP_TYPES_VALUES),
-
-    /**
-     * Add a marker on the center
-     */
-    hasCenterMarker: React.PropTypes.bool,
-  }
-
-  static defaultProps = {
-    format: IMAGE_FORMATS.PNG,
-    mapType: MAP_TYPES.ROADMAP,
-    hasCenterMarker: true,
-  }
-
-  render() {
-    return (
-      <img
-        style={[this.props.style, this.props.size]}
-        src={this.staticMapUrl}
-      />
-    );
-  }
-
-  get staticMapUrl() {
-    const {
-      latitude,
-      longitude,
-      zoom,
-      size,
-      scale,
-      format,
-      mapType,
-      } = this.props;
-
-    const {width, height} = size;
-    const rootUrl = this.constructor.RootUrl;
-
-    return `${rootUrl}?center=${latitude},${longitude}&zoom=${zoom}&scale=${scale}&size=${width}x${height}&maptype=${mapType}&format=${format}&${this.markerParams}&${this.apiKeyParam}`;
-  }
-
-  get markerParams() {
-    const {
-      latitude,
-      longitude,
-      hasCenterMarker,
-      } = this.props;
-
-    const markerParams = `markers=icon:dot%7Cshadow:true%7C${latitude},${longitude}`;
-    return hasCenterMarker ? markerParams : '';
-  }
-
-  get apiKeyParam() {
-    const apiKey = this.constructor.ApiKey;
-    return apiKey ? `key=${apiKey}` : '';
-  }
-}
-
-
-export default GoogleStaticMap;
+  /**
+   * Add a marker on the center
+   */
+  hasCenterMarker: _react2.default.PropTypes.bool
+};
+GoogleStaticMap.defaultProps = {
+  format: IMAGE_FORMATS.PNG,
+  mapType: MAP_TYPES.ROADMAP,
+  hasCenterMarker: true
+};
+exports.default = GoogleStaticMap;
